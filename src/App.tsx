8d29d451ec36dfe40c5db18cfc1a6017b825d526
ylu <ylu@ert.com>
2021-08-20 21:27:54 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,15 @@
-import React from 'react'
-import Counter from './features/counter/Counter'
-import Dogs from './features/dogs/Dogs'
-import logo from './logo.svg'
-import './App.css'
+import React from "react"
+import Counter from "./features/counter/Counter"
+import Dogs from "./features/dogs/Dogs"
+import logo from "./logo.svg"
+import "./App.css"
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-
+    <div className='App'>
+      <header className='App-header'>
         <Counter />
-
         <Dogs />
-
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
       </header>
     </div>
   )
--- src/features/counter/couter-slice.ts
+++ src/features/counter/couter-slice.ts
@@ -1,7 +1,7 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
 interface CounterState {
-  value: number;
+  value: number
 }
 
 const initialState: CounterState = {
@@ -9,7 +9,7 @@ const initialState: CounterState = {
 }
 
 const counterSlice = createSlice({
-  name: 'Counter',
+  name: "counter",
   initialState,
   reducers: {
     incremented: (state) => {
@@ -22,13 +22,15 @@ const counterSlice = createSlice({
 
     amountAdded: (state, action: PayloadAction<number>) => {
       state.value += action.payload
-    }
-  }
-});
+    },
+  },
+})
 
 export const { incremented, decremented, amountAdded } = counterSlice.actions
 
+console.log("incremented=", incremented()) // {type: "counter/incremented", payload: undefined}
+console.log("decremented=", decremented()) // {type: "counter/decremented", payload: undefined}
+console.log("amountAdded=", amountAdded(2)) // {type: "counter/amountAdded", payload: 2}
+
 export default counterSlice.reducer
-
-
-
+console.log("counterSlice.reducer=", counterSlice.reducer) // (state, action) => {}
